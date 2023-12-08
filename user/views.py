3a55c63bcd271641a3812c72795b7c1f80f25277
Ravshanbekk07 <ramazonof07@gmail.com>
2023-12-08 21:58:18 +0500

    registerview

--- user/views.py
+++ user/views.py
@@ -5,4 +5,11 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-
+@api_view(['POST'])
+def register_user(request):
+    if request.method=='POST':
+        serializer=CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
